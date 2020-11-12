9067aa2e82aa330e09026e8889b8ef3171e622ea
LiudmylaKostova <kostolu@ukr.net>
2020-11-12 21:21:30 +0300

    task4 done

---
+++ js/task-04.js
@@ -0,0 +1,28 @@
+//Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
+
+//Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+//Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+//Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+
+let counterValue = 0;
+
+const decrementRef = document.querySelector('[data-action="decrement"]');
+const incrementRef = document.querySelector('[data-action="increment"]');
+const valueRef = document.querySelector("#value");
+
+const increment = () => {
+    counterValue += 1;
+    valueRef.textContent = counterValue;
+}
+
+
+const decrement = () => {
+    counterValue -= 1;
+    valueRef.textContent = counterValue;
+}
+
+
+decrementRef.addEventListener("click", decrement);
+incrementRef.addEventListener("click", increment);
+
